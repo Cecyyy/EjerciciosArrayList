bc5db3286afbb1453411d3b7f8574e36e25b59a3
MAC20_LC3 <mac20_lc3@iMac-de-MAC20-LC3.local>
2022-03-28 11:55:53 -0600

    trabajo en clase

---
+++ src/Uno.java
@@ -0,0 +1,18 @@
+import java.util.List;
+
+public class Uno {
+    public static void main(String[] args) {
+        public static List <Interger> compareTriplets (List<Integer>a,List <Integer>b){
+            int contA=0;
+            int contB=0;
+            for (int i = 0; i < a.size; i++) {
+                if (a.get (i)<b.get(i)){
+                    contA++;
+                }else if{
+                    
+                }
+            }
+        }
+        )
+    }
+}
